ddf8e3c00fe89d31492ff0f4290e2a11a81df148
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-17 10:14:52 -0400

    tictactoe

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -5,33 +5,84 @@ package rocks.zipcode.io.quiz3.arrays;
  */
 public class TicTacToe {
 
+    String[][] board;
+
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
+        board = new String[0][0];
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] column = new String[board.length];
+        for (int i = 0; i < board.length; i++){
+            column[i] = board[i][value];
+        }
+        return column;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        if(rowIndex == 0 && board[0][0].equals(board[0][1]) && board[0][1].equals(board[0][2])){
+            return true;
+        }
+        if(rowIndex == 1 && board[1][0].equals(board[1][1]) && board[1][1].equals(board[1][2])){
+            return true;
+        }
+        if(rowIndex == 2 && board[2][0].equals(board[2][1]) && board[2][1].equals(board[2][2])){
+            return true;
+        }
+        else return false;
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        if(columnIndex == 0 && board[0][0].equals(board[1][0]) && board[1][0].equals(board[2][0])){
+            return true;
+        }
+        if(columnIndex == 1 && board[0][1].equals(board[1][1]) && board[1][1].equals(board[2][1])){
+            return true;
+        }
+        if(columnIndex == 2 && board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2])){
+            return true;
+        }
+        else return false;
     }
 
     public String getWinner() {
+        if(board[0][0].equals("O") && board[0][1].equals("O") && board[0][2].equals("O")){
+            return board[0][2];
+        }
+        if(board[1][0].equals("O") && board[1][1].equals("O") && board[1][2].equals("O")){
+            return board[1][2];
+        }
+        if(board[2][0].equals("O") && board[2][1].equals("O") && board[2][2].equals("O")){
+            return board[2][2];
+        }
+        if(board[0][0].equals("X") && board[1][0].equals("X") && board[2][0].equals("X")){
+            return board[0][2];
+        }
+        if(board[0][1].equals("X") && board[1][1].equals("X") && board[2][1].equals("X")){
+            return board[1][1];
+        }
+        if(board[0][2].equals("X") && board[1][2].equals("X") && board[2][2].equals("X")){
+            return board[2][0];
+        }
+        if(board[0][0].equals("X") && board[1][1].equals("X") && board[2][2].equals("X")){
+            return board[0][2];
+        }
+        if(board[2][0].equals("X") && board[1][1].equals("X") && board[0][2].equals("X")){
+            return board[2][0];
+        }
+
         return null;
     }
 
     public String[][] getBoard() {
-        return null;
+        return board;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -4,7 +4,17 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
+
     public String translate(String str) {
-        return null;
+        String output = "";
+        if(VowelUtils.startsWithVowel(str)){
+            output = str + "way";
+        } else if (VowelUtils.hasVowels(str)){
+            output = str + "ay";
+        } else {
+            return null;
+        }
+
+        return output;
     }
 }
